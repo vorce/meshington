7f333ec8424721bfbf9ac3c3c8a511e861178ea8
Joel C <joel@vorce.se>
2018-05-21 13:58:33 +0200

    Add structs for local state

---
+++ lib/meshington/database.ex
@@ -0,0 +1,16 @@
+defmodule Meshington.Database do
+  @moduledoc """
+  Keeps local state in memory
+  """
+
+  use GenServer
+
+  def start_link() do
+  end
+
+  def init() do
+  end
+
+  def update(%Meshington.Login{} = login) do
+  end
+end
---
+++ lib/meshington/identity.ex
@@ -0,0 +1,22 @@
+defmodule Meshington.Identity do
+  @moduledoc """
+  Identifies a meshington client/node.
+  """
+  defstruct name: "", uuid: ""
+
+  def new(name) do
+    %__MODULE__{
+      name: name,
+      uuid: mac_address()
+    }
+  end
+
+  def mac_address() do
+    with {:ok, addrs} <- :inet.getifaddrs() do
+      addrs
+      |> Enum.filter(fn {name, _} -> name == 'en0' end)
+      |> Keyword.get(:hwaddr, [])
+      |> Enum.join()
+    end
+  end
+end
